682333900a5e6022610e03557b3829121c818611
UnaLai <qqq323618@gmail.com>
2020-05-31 23:20:25 +0800

    [feat]:add switch transition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <button @click="isOpen = !isOpen">Control</button>
-    <transition name="fade">
+    <!-- <transition name="fade">
       <p v-if="isOpen">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, ea.
         Adipisci dignissimos repellat eaque dolor provident incidunt, cumque
         aperiam quod repudiandae, tempora ea nobis laboriosam quis totam
         quibusdam in enim!
       </p>
+    </transition> -->
+    <transition name="fade" mode="out-in">
+      <p v-if="isOpen">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, ea.
+        Adipisci dignissimos repellat eaque dolor provident incidunt.
+      </p>
+      <div v-else class="box"></div>
     </transition>
   </div>
 </template>
@@ -29,22 +36,29 @@ button {
   border: 2px solid #005fcc;
   padding: 5px;
   border-radius: 5px;
-  transition: background-color 300ms;
+  margin-bottom: 10px;
 }
 p {
   width: 300px;
-  height: 300px;
   padding: 20px;
+  margin: 0 auto;
+}
+.box {
+  width: 300px;
+  height: 100px;
+  background-color: pink;
   margin: 0 auto;
 }
 .fade-enter {
   opacity: 0;
+  transform: translateX(-10px);
 }
 .fade-enter-active,
 .fade-leave-active {
-  transition: all 2s;
+  transition: all 1s;
 }
 .fade-leave-to {
   opacity: 0;
+  transform: translateX(-10px);
 }
 </style>
